e8896326f56af564ebadebf0a3c405cc71109511
Jonnyolsen1 <63315119+Jonnyolsen1@users.noreply.github.com>
2022-05-14 18:10:24 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,13 @@
+package gr.upatras.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//this is a Spring Application
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
+	}
+}
