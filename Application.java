9aa872933b42e140540f425023ae15d3ae1929ec
Jonnyolsen1 <63315119+Jonnyolsen1@users.noreply.github.com>
2022-05-14 18:17:36 +0300

    Update Application.java
    Added a test comment

--- Application.java
+++ Application.java
@@ -3,6 +3,7 @@ package gr.upatras.rest.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this is a Spring Application
 @SpringBootApplication
 public class Application {
 	
